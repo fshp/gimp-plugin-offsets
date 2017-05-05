e969acd33cdbb83fff89b70f7635a51fd802ccc7
Maxim Moseychuk <franchesko.salias.hudro.pedros@gmail.com>
2017-05-05 20:52:30 +0300

    Its work!

--- offsets.c
+++ offsets.c
@@ -31,46 +31,75 @@ query (void)
       GIMP_PDB_IMAGE,
       "image",
       "Input image"
-    },
-    {
-      GIMP_PDB_DRAWABLE,
-      "drawable",
-      "Input drawable"
     }
   };
 
   gimp_install_procedure (
     "get-layers-offset",
-    "Hello, world!",
-    "Displays \"Hello, world!\" in a dialog",
-    "fshp",
-    "Copyright fshp",
+    "Displays layers offsets in a dialog",
+    "Displays layers offsets in a dialog",
+    "Maxim Moseychuk",
+    "Copyright Maxim Moseychuk",
     "2017",
-    "_Hello world...",
-    "RGB*, GRAY*",
+    "Get offsets",
+    "RGB*, GRAY*, INDEXED*",
     GIMP_PLUGIN,
-    G_N_ELEMENTS (args), 0,
+    G_N_ELEMENTS(args), 0,
     args, NULL);
 
   gimp_plugin_menu_register ("get-layers-offset",
-                             "<Image>/Filters/Misc");
+                             "<Image>/Filters/Layers");
+}
+
+static GString* get_padding(guint padding) {
+  GString *result = g_string_new(NULL);
+  while (padding) {
+    g_string_append_c(result, '\t');
+    --padding;
+  }
+  return result;
 }
 
 static GString* unsupported_process(const guint deph) {
   GString *result = g_string_new(NULL);
-
+  GString *padding = get_padding(deph);
+  g_string_printf(result, "%sunsupported item", padding->str);
+  g_string_free(padding, TRUE);
   return result;
 }
 
-static GString* layer_process(const gint layer, const guint deph) {
+static GString* drawable_process(const gint layer, const guint deph) {
   GString *result = g_string_new(NULL);
-
+  GString *padding = get_padding(deph);
+  gint x, y;
+  gimp_drawable_offsets(layer, &x, &y);
+  g_string_printf(result, "%s%s: %i %i", padding->str, gimp_drawable_get_name(layer), x, y);
+  g_string_free(padding, TRUE);
   return result;
 }
 
 static GString* group_process(const gint group, const guint deph) {
   GString *result = g_string_new(NULL);
+  GString *padding = get_padding(deph);
+  g_string_printf(result, "%s%s:", padding->str, gimp_item_get_name(group));
+  gint num_children;
+  gint *childrens = gimp_item_get_children(group, &num_children);
 
+  for(gint i = 0; i < num_children; ++i) {
+    GString *r = NULL;
+    const gint item = childrens[i];
+    if(gimp_item_is_group(item)) {
+      r = group_process(item, deph + 1);
+    } else if(gimp_item_is_drawable(item)) {
+      r = drawable_process(item, deph + 1);
+    } else {
+      r = unsupported_process(deph + 1);
+    }
+    g_string_append(g_string_append_c(result, '\n'), r->str);
+    g_string_free(r, TRUE);
+  }
+
+  g_string_free(padding, TRUE);
   return result;
 }
 
@@ -85,8 +114,8 @@ static GString* image_process(const gint image) {
     const gint item = layers[i];
     if(gimp_item_is_group(item)) {
       r = group_process(item, deph + 1);
-    } else if(gimp_item_is_layer(item)) {
-      r = layer_process(item, deph + 1);
+    } else if(gimp_item_is_drawable(item)) {
+      r = drawable_process(item, deph + 1);
     } else {
       r = unsupported_process(deph + 1);
     }
@@ -104,28 +133,19 @@ run (const gchar      *name,
      gint             *nreturn_vals,
      GimpParam       **return_vals)
 {
-  static GimpParam  values[1];
-  GimpPDBStatusType status = GIMP_PDB_SUCCESS;
+  // static GimpParam  values[1];
+  // GimpPDBStatusType status = GIMP_PDB_SUCCESS;
   GimpRunMode       run_mode;
 
   /* Setting mandatory output values */
-  *nreturn_vals = 1;
-  *return_vals  = values;
+  // *nreturn_vals = 1;
+  // *return_vals  = values;
 
-  values[0].type = GIMP_PDB_STATUS;
-  values[0].data.d_status = status;
+  // values[0].type = GIMP_PDB_STATUS;
+  // values[0].data.d_status = status;
 
   const gint32 image = param[1].data.d_image;
   GString *msg = image_process(image);
-  // gint layers_num;
-  // gint *layers = gimp_image_get_layers(image, &layers_num);
-  // gboolean b = gimp_item_is_group(layers[0]);
-  //
-  // gint num_chirldren;
-  // gint *items = gimp_item_get_children(layers[1], &num_chirldren);
-
-  /* Getting run_mode - we won't display a dialog if
-   * we are in NONINTERACTIVE mode */
   run_mode = param[0].data.d_int32;
 
   if (run_mode != GIMP_RUN_NONINTERACTIVE)
